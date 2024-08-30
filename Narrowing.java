df3852523f3112d58bd472ecb6356edcd1877ed8
SuchitaChavan <158129439+SuchitaChavan@users.noreply.github.com>
2024-08-30 19:09:39 +0530

    Add files via upload

---
+++ Narrowing.java
@@ -0,0 +1,13 @@
+class Narrowing
+{
+  public static void main(String args[])
+   {
+	double N1 = 15;				//Create Double type variable
+        System.out.println("The Double Value Is:"+N1);
+       
+    	//Converting Double Into Int Type
+
+         int Num = (int) N1;
+          System.out.println("The Int Value Is:"+Num);
+       }
+}
