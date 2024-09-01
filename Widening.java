365993ebf65f3bcff76713c770ae4c605dc5193f
SuchitaChavan <158129439+SuchitaChavan@users.noreply.github.com>
2024-09-01 17:07:13 +0530

    Add files via upload

---
+++ Widening.java
@@ -0,0 +1,14 @@
+class Widening
+{
+  public static void main(String args[])
+   {
+     int N1 = 15;   		        			//Create Int type variable
+      System.out.println("The Integer Value Is:"+N1);
+
+      //Convert Int Into Double
+
+       double Num = N1;
+      System.out.println("The Double Value Is:"+Num);
+    }
+}
+       
